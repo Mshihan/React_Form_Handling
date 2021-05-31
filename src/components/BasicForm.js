4f21f0c38b6ca866fadb1d00c316b00ee61a09c8
Mshihan <167sihan@gmail.com>
2021-05-31 17:56:01 +0530

    Simple Form Validation with custom hook final commit

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,21 +1,23 @@
 const BasicForm = (props) => {
+  const onSubmitFormHandler = (event) => {};
+
   return (
-    <form>
-      <div className='control-group'>
-        <div className='form-control'>
-          <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+    <form onSubmit={onSubmitFormHandler}>
+      <div className="control-group">
+        <div className="form-control">
+          <label htmlFor="name">First Name</label>
+          <input type="text" id="name" />
         </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+        <div className="form-control">
+          <label htmlFor="name">Last Name</label>
+          <input type="text" id="name" />
         </div>
       </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+      <div className="form-control">
+        <label htmlFor="name">E-Mail Address</label>
+        <input type="text" id="name" />
       </div>
-      <div className='form-actions'>
+      <div className="form-actions">
         <button>Submit</button>
       </div>
     </form>
