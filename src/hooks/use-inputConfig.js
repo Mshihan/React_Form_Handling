ac00aeba918141c51ca1e8149c3aaea219b60dd5
Mshihan <167sihan@gmail.com>
2021-05-31 20:32:39 +0530

    useState to useReducer conversion

--- src/hooks/use-inputConfig.js
+++ src/hooks/use-inputConfig.js
@@ -1,31 +1,71 @@
-import { useState } from "react";
+// import { useState } from "react";
+import { useReducer } from "react";
+
+const initalInputState = {
+  value: "",
+  isTouched: false,
+};
+
+const inputStateReducer = (state, action) => {
+  if (action.type === "INPUT") {
+    return {
+      ...state,
+      value: action.value,
+    };
+  }
+  if (action.type === "BLUR") {
+    return {
+      ...state,
+      isTouched: true,
+    };
+  }
+  if (action.type === "RESET") {
+    return {
+      value: "",
+      isTouched: false,
+    };
+  }
+  return inputStateReducer;
+};
+
 ///Custom React Hook used to validate use inputs in client side
 const useInputConfig = (validation) => {
-  const [value, setValue] = useState("");
-  const [isTouched, setIsTouched] = useState(false);
+  const [inputState, dispatch] = useReducer(
+    inputStateReducer,
+    initalInputState
+  );
+  // const [value, setValue] = useState("");
+  // const [isTouched, setIsTouched] = useState(false);
 
-  const validationRule = validation(value);
-  const hasError = validationRule && isTouched;
+  const validationRule = validation(inputState.value);
+  const hasError = validationRule && inputState.isTouched;
 
   const onInputChangeHandler = (event) => {
-    setValue(event.target.value);
+    // setValue(event.target.value);
+    dispatch({
+      type: "INPUT",
+      value: event.target.value,
+    });
   };
 
   const onBlurTextFieldHandler = (event) => {
-    setIsTouched(true);
+    dispatch({
+      type: "BLUR",
+    });
   };
 
   const reset = () => {
-    setValue("");
-    setIsTouched(false);
+    dispatch({
+      type: "RESET",
+    });
   };
 
   return {
-    onInputChangeHandler,
-    onBlurTextFieldHandler,
-    hasError,
-    value,
-    reset,
+    onInputChangeHandler: onInputChangeHandler,
+    onBlurTextFieldHandler: onBlurTextFieldHandler,
+    hasError: hasError,
+    value: inputState.value,
+    reset: reset,
   };
 };
 
