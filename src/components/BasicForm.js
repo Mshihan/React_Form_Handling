fff71b2b60daf471907797e15d4e21520b4d4cd2
Mshihan <167sihan@gmail.com>
2021-05-31 19:25:56 +0530

    Basic Form Validation Implementation using custom hooks

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,21 +1,103 @@
+import useInputConfig from "./../hooks/use-inputConfig";
+
 const BasicForm = (props) => {
-  const onSubmitFormHandler = (event) => {};
+  const {
+    onInputChangeHandler: onFirstNameInputChangeHandler,
+    onBlurTextFieldHandler: onBlurFirstName,
+    hasError: hasErrorFirstName,
+    value: firstName,
+    reset: resetFirstName,
+  } = useInputConfig((value) => {
+    return value.trim() === "";
+  });
+
+  const {
+    onInputChangeHandler: onLastNameInputChangeHandler,
+    onBlurTextFieldHandler: onBlurLastNameHander,
+    hasError: hasErrorLastName,
+    value: lastName,
+    reset: resetLastName,
+  } = useInputConfig((value) => {
+    return value.trim() === "";
+  });
+
+  const {
+    onInputChangeHandler: onEmailInputChangeHandler,
+    onBlurTextFieldHandler: onBlurEmailHander,
+    hasError: hasErrorEmail,
+    value: email,
+    reset: resetEmail,
+  } = useInputConfig((enteredEmail) => {
+    return !enteredEmail.match(
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    );
+  });
+
+  const validForm = !hasErrorFirstName && !hasErrorLastName && !hasErrorEmail;
+
+  const onSubmitFormHandler = (event) => {
+    event.preventDefault();
+    if (!validForm) {
+      return;
+    }
+    console.log(firstName + "\n" + lastName + "\n" + email);
+    resetFirstName();
+    resetLastName();
+    resetEmail();
+  };
+
+  const classesFirstName = hasErrorFirstName
+    ? "form-control invalid"
+    : "form-control";
+
+  const classesLastName = hasErrorLastName
+    ? "form-control invalid"
+    : "form-control";
+
+  const classesEmail = hasErrorEmail ? "form-control invalid" : "form-control";
 
   return (
     <form onSubmit={onSubmitFormHandler}>
       <div className="control-group">
-        <div className="form-control">
+        <div className={classesFirstName}>
           <label htmlFor="name">First Name</label>
-          <input type="text" id="name" />
+          <input
+            type="text"
+            id="name"
+            value={firstName}
+            onChange={onFirstNameInputChangeHandler}
+            onBlur={onBlurFirstName}
+          />
+          {hasErrorFirstName && (
+            <p className="error-text">**First Name must not be empty</p>
+          )}
         </div>
-        <div className="form-control">
+        <div className={classesLastName}>
           <label htmlFor="name">Last Name</label>
-          <input type="text" id="name" />
+          <input
+            type="text"
+            value={lastName}
+            id="name"
+            onChange={onLastNameInputChangeHandler}
+            onBlur={onBlurLastNameHander}
+          />
+          {hasErrorLastName && (
+            <p className="error-text">**Last Name must not be empty</p>
+          )}
         </div>
       </div>
-      <div className="form-control">
+      <div className={classesEmail}>
         <label htmlFor="name">E-Mail Address</label>
-        <input type="text" id="name" />
+        <input
+          value={email}
+          type="text"
+          id="name"
+          onChange={onEmailInputChangeHandler}
+          onBlur={onBlurEmailHander}
+        />
+        {hasErrorEmail && (
+          <p className="error-text">**Email must not be empty</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
--- src/hooks/use-inputConfig.js
+++ src/hooks/use-inputConfig.js
@@ -1,7 +1,32 @@
-import React, { useState } from "react";
+import { useState } from "react";
+///Custom React Hook used to validate use inputs in client side
+const useInputConfig = (validation) => {
+  const [value, setValue] = useState("");
+  const [isTouched, setIsTouched] = useState(false);
 
-const useInputConfig = (validations) => {
-  return {};
+  const validationRule = validation(value);
+  const hasError = validationRule && isTouched;
+
+  const onInputChangeHandler = (event) => {
+    setValue(event.target.value);
+  };
+
+  const onBlurTextFieldHandler = (event) => {
+    setIsTouched(true);
+  };
+
+  const reset = () => {
+    setValue("");
+    setIsTouched(false);
+  };
+
+  return {
+    onInputChangeHandler,
+    onBlurTextFieldHandler,
+    hasError,
+    value,
+    reset,
+  };
 };
 
 export default useInputConfig;
